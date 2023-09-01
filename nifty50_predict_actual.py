d3b163f0b5d8fe1c2576566d9c515834b8227695
tnagarajesh <140355573+tnagarajesh@users.noreply.github.com>
2023-09-01 01:12:26 -0400

    Add files via upload

--- nifty50_predict_actual.py
+++ nifty50_predict_actual.py
@@ -6,18 +6,16 @@ nifty50 = pd.read_csv('https://storage.googleapis.com/nifty50/Nifty50_prediction
 nifty50_pre = pd.read_csv('https://storage.googleapis.com/nifty50/nifty50_features_predictions.csv')
 
 st.write("Today Nifty50 predicted close price:", nifty50_pre.iat[-1,7])
-st.table(nifty50_pre)
 
 # create data
-x = nifty50_pre['Date']
-x1 = nifty50['Date']
+x = nifty50['Date']
 z = nifty50['Actual_Nifty50_Close']
-p = nifty50_pre['Predicted_Value']
+p = nifty50['Predicted_Nifty50_Close']
 e = nifty50['Prediction Error (Points)']
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 
-ax1.plot(x1, z, label="Actual Close Value", marker='o', linestyle="-")
+ax1.plot(x, z, label="Actual Close Value", marker='o', linestyle="-")
 ax1.plot(x, p, label="Predicted Close Value", marker='o', linestyle="--")
 ax1.legend()
 
