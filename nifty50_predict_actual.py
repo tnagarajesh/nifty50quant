83971326e777944f2431c17da49ceac9c689020b
tnagarajesh <140355573+tnagarajesh@users.noreply.github.com>
2023-10-07 14:36:04 -0400

    Update nifty50_predict_actual.py

--- nifty50_predict_actual.py
+++ nifty50_predict_actual.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import plotly.express as px
 
-nifty50 = pd.read_csv('https://storage.googleapis.com/nifty50_prediction/nifty50_prediction.csv')
+st.set_page_config(page_title="NiftyQuants", layout="centered")
+
+nifty50 = pd.read_csv('https://storage.googleapis.com/nifty50_prediction/nifty50_prediction.csv',index_col=None)
 nifty50_pre = pd.read_csv('https://storage.googleapis.com/nifty50_prediction/nifty50_features_predictions.csv')
 
+st.write("Today Nifty50 predicted close:", nifty50_pre.iat[-1,7])
 
-st.write("Today Nifty50 predicted close price:", nifty50_pre.iat[-1,7])
-
-st.write("Past Predicted Vs Actual Nifty50 close price comparison:")
+st.write("Past Predicted Vs Actual Nifty50 close comparison:")
 
 # create data
 x = nifty50['Date']
@@ -16,13 +18,13 @@ z = nifty50['Actual_Nifty50_Close']
 p = nifty50['Predicted_Nifty50_Close']
 e = nifty50['Prediction Error (Points)']
 
-fig, (ax1, ax2) = plt.subplots(2, 1)
+fig, (ax1) = plt.subplots(1, 1)
 
 ax1.plot(x, z, label="Actual Close Value", marker='o', linestyle="-")
 ax1.plot(x, p, label="Predicted Close Value", marker='o', linestyle="--")
 ax1.legend()
-
-ax2.plot(x, e, label="Prediction Error(points) - 0 means no error", marker='o', linestyle="-")
-ax2.legend()
-
 st.pyplot(fig)
+
+st.write(nifty50)
+
+
