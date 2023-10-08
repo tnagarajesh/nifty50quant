62ffb6bc3529057f37a6231f46729d57f02dc212
tnagarajesh <140355573+tnagarajesh@users.noreply.github.com>
2023-10-08 16:12:44 -0400

    Update nifty50_predict_actual.py

--- nifty50_predict_actual.py
+++ nifty50_predict_actual.py
@@ -23,19 +23,19 @@ z = nifty50['Actual_Nifty50_Close']
 p = nifty50['Predicted_Nifty50_Close']
 e = nifty50['Prediction Error (Points)']
 
-graph, table = st.columns(2)
+#graph, table = st.columns(2)
 
-with graph:
-    fig = plt.figure(figsize=(15, 10), dpi=120)
-    plt.plot(x, z, label="Nifty50 Actual Close", marker="o", markersize=10, linestyle="-")
-    plt.plot(x, p, label="Past Nifty50 Predicted Close", marker="o", markersize=10, linestyle="--")
-    plt.plot("Today Nifty50 Close", nifty50_pred_value, label="Today Nifty50 Close", marker="o", markersize=10)
-    plt.title("Past Predicted Vs Actual Nifty50 Close")
-    plt.grid(visible=None, which='major', axis='y', linestyle='--')
-    # plt.xticks(rotation='vertical', fontsize=8)
-    plt.legend()
-    st.pyplot(fig)
+#with graph:
+fig = plt.figure(figsize=(8, 4), dpi=120)
+plt.plot(x, z, label="Nifty50 Actual Close", marker="o", markersize=10, linestyle="-")
+plt.plot(x, p, label="Past Nifty50 Predicted Close", marker="o", markersize=10, linestyle="--")
+plt.plot("Today Nifty50 Close", nifty50_pred_value, label="Today Nifty50 Close", marker="o", markersize=10)
+plt.title("Past Predicted Vs Actual Nifty50 Close")
+plt.grid(visible=None, which='major', axis='y', linestyle='--')
+# plt.xticks(rotation='vertical', fontsize=8)
+plt.legend()
+st.pyplot(fig)
 
-with table:
-    st.write("")
-    st.dataframe(nifty50, use_container_width=True, hide_index=True)
+#with table:
+st.write("")
+st.dataframe(nifty50, use_container_width=True, hide_index=True)
